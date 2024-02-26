94c4d5b9a72ffe7f7ecb6a23b3ef4cac5a36e935
Ana R. Carvalho <137975286+AnaRaquelCarvalho@users.noreply.github.com>
2024-02-26 15:53:50 -0300

    jogo do Adivinha
    Atualizado

--- JogoAdivinha.py
+++ JogoAdivinha.py
@@ -1,13 +1,21 @@
 print('-'*36)
 print('{:^16}'.format('Jogo Adinha e Número por Extenso'))
 print('-'*36)
-n = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
+from time import sleep
+n = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 
+     'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze',
+      'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito',
+       'dezenove', 'vinte')
 while True:
     num = int(input('Digite um número entre 0 e 20: '))
+    print('-'*36)
     if num < 0 or num > 20:
         print('Invalido Tente novamente ', end='')
     else:
-        print(f'Você digitou o número {n[num]}')
+        print('{:^30}'.format('Você digitou o número...'))
+        print('{:^30}'.format('PROCESSANDO...'))
+        sleep(3)
+        print(f'    Número >>> {n[num]}')
         print('-'*36)
     resp = ' '
     while resp not in 'SN':
